f285a642ca3042f1945fb2a71c5c1273fcbf2026
henglinli <henglinli@gmail.com>
2015-11-02 00:03:39 +0800

    add thread

---
+++ test/thread_unittest.cpp
@@ -0,0 +1,27 @@
+// -*-coding:utf-8-unix;-*-
+#include "gtest/gtest.h"
+#include "thread.hh"
+//
+class Task {
+ public:
+  explicit Task(int value)
+      : _value(value) {
+  }
+  //
+  int* Loop() {
+    return &_value;
+  }
+  //
+ private:
+  int _value;
+};
+//
+TEST(thread, task) {
+  base::Thread<Task> thread;
+  Task task(1);
+  int done(-1);
+  done = thread.Run(&task);
+  ASSERT_EQ(0, done);
+  int value = thread.Join();
+  EXPECT_EQ(1, value);
+}
---
+++ thread.hh
@@ -0,0 +1,41 @@
+// -*-coding:utf-8-unix;-*-
+#pragma once
+#include <pthread.h>
+#include <stdint.h>
+#include "nullptr.hh"
+//
+namespace base {
+//
+class Task {
+ public:
+  virtual int* Loop() = 0;
+};
+//
+template<typename Task>
+class Thread {
+ public:
+  int Run(Task* task) {
+    return pthread_create(&_tid, nullptr,
+                          ThreadMain, static_cast<void*>(task));
+  }
+  //
+  int Join() {
+    int done = pthread_join(_tid, reinterpret_cast<void**>(&_result));
+      if (0 == done) {
+        return (*_result);
+      }
+  }
+  //
+ protected:
+  static void* ThreadMain(void* arg) {
+    Task* task = static_cast<Task*>(arg);
+    return task->Loop();
+  }
+  //
+ private:
+  int* _result;
+  pthread_t _tid;
+  pthread_attr_t _attr;
+}; // class Thread
+//
+} // namespace base
