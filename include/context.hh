36d1f6924605d6547281d78890d7da27db0f16ba
henglinli <henglinli@gmail.com>
2015-12-07 23:26:35 +0800

    remove Heap

--- include/context.hh
+++ include/context.hh
@@ -7,11 +7,6 @@
 #define _XOPEN_SOURCE 700
 #endif // __APPLE__
 //
-#define MMAP
-#ifdef MMAP
-#include <sys/mman.h>
-#endif // MMAP
-//
 #include <ucontext.h>
 #include <setjmp.h>
 #include "macros.hh"
@@ -23,19 +18,6 @@ namespace NAMESPACE {
 //     : public Context<_ContextTask, 1> {
 //   void Run();
 // };
-//
-class Heap {
- public:
-  Heap();
-  //
-  ~Heap();
-  //
-  void* Mmap(size_t size);
-  //
- private:
-  void* _data;
-  size_t _size;
-};
 //
 const size_t kDefaultStackSize(SIGSTKSZ);
 //
@@ -52,45 +34,12 @@ class Context {
   static void Routine(Context* context);
   //
  private:
-  Heap _stack;
+  char _stack[kStackSize];
   jmp_buf _jmpbuf;
 };
 //
-Heap::Heap()
-    : _data(nullptr)
-    , _size(0) {
-  // nil
-}
-//
-Heap::~Heap() {
-  if (nullptr == _data or 0 == _size) {
-#ifdef MMAP
-    munmap(_data, _size);
-#else // MMAP
-    free(_data);
-#endif // MMAP
-  }
-}
-//
-void* Heap::Mmap(size_t size) {
-  if (0 < size) {
-#ifdef MMAP
-    _size = size;
-    _data = mmap(nullptr, size, PROT_EXEC bitor PROT_READ bitor PROT_WRITE,
-                 MAP_PRIVATE bitor MAP_ANONYMOUS, -1, 0);
-#else // MMAP
-    _data = malloc(size);
-#endif // MMAP
-  }
-  return _data;
-}
-//
 template<typename Task, size_t kStackSize>
 bool Context<Task, kStackSize>::Init(Context& context) {
-  void* stack = context._stack.Mmap(kStackSize);
-  if (nullptr == stack) {
-    return false;
-  }
   typedef Context<Task, kStackSize> Self;
   ucontext_t initial_context;
   int done = getcontext(&initial_context);
@@ -98,8 +47,8 @@ bool Context<Task, kStackSize>::Init(Context& context) {
     return false;
   }
   //
-  initial_context.uc_stack.ss_sp = stack;
-  initial_context.uc_stack.ss_size = kStackSize;
+  initial_context.uc_stack.ss_sp = context._stack;
+  initial_context.uc_stack.ss_size = sizeof(context._stack);
   initial_context.uc_link = nullptr;
   //
   makecontext(&initial_context,
